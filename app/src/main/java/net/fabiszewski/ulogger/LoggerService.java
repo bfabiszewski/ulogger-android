91fa852df5f9e0a7bfa1820d245ba33eddddedaf
Bartek Fabiszewski <github@fabiszewski.net>
2024-04-06 17:14:14 +0200

    Fix: on API >= 34 broadcast intents must have explicit package

--- app/src/main/java/net/fabiszewski/ulogger/LoggerService.java
+++ app/src/main/java/net/fabiszewski/ulogger/LoggerService.java
@@ -92,7 +92,7 @@ public class LoggerService extends Service {
             locationHelper.updatePreferences();
             locationHelper.requestLocationUpdates(locationListener, looper);
             setRunning(true);
-            sendBroadcast(BROADCAST_LOCATION_STARTED);
+            BroadcastHelper.sendBroadcast(this, BROADCAST_LOCATION_STARTED);
 
             syncIntent = new Intent(getApplicationContext(), WebSyncService.class);
 
@@ -103,9 +103,9 @@ public class LoggerService extends Service {
         } catch (LocationHelper.LoggerException e) {
             int errorCode = e.getCode();
             if (errorCode == E_DISABLED) {
-                sendBroadcast(BROADCAST_LOCATION_DISABLED);
+                BroadcastHelper.sendBroadcast(this, BROADCAST_LOCATION_DISABLED);
             } else if (errorCode == E_PERMISSION) {
-                sendBroadcast(BROADCAST_LOCATION_PERMISSION_DENIED);
+                BroadcastHelper.sendBroadcast(this, BROADCAST_LOCATION_PERMISSION_DENIED);
             }
         }
         return false;
@@ -179,7 +179,7 @@ public class LoggerService extends Service {
         setRunning(false);
 
         notificationHelper.cancelNotification();
-        sendBroadcast(BROADCAST_LOCATION_STOPPED);
+        BroadcastHelper.sendBroadcast(this, BROADCAST_LOCATION_STOPPED);
 
         if (thread != null) {
             thread.interrupt();
@@ -233,15 +233,6 @@ public class LoggerService extends Service {
     }
 
     /**
-     * Send broadcast message
-     * @param broadcast Broadcast message
-     */
-    private void sendBroadcast(String broadcast) {
-        Intent intent = new Intent(broadcast);
-        sendBroadcast(intent);
-    }
-
-    /**
      * Location listener class
      */
     private class mLocationListener implements LocationListener {
@@ -259,7 +250,7 @@ public class LoggerService extends Service {
             if (meetsCriteria(location)) {
                 lastLocation = location;
                 DbAccess.writeLocation(LoggerService.this, location);
-                sendBroadcast(BROADCAST_LOCATION_UPDATED);
+                BroadcastHelper.sendBroadcast(LoggerService.this, BROADCAST_LOCATION_UPDATED);
                 if (locationHelper.isLiveSync()) {
                     getApplicationContext().startService(syncIntent);
                 }
@@ -309,12 +300,12 @@ public class LoggerService extends Service {
         public void onProviderDisabled(@NonNull String provider) {
             if (Logger.DEBUG) { Log.d(TAG, "[location provider " + provider + " disabled]"); }
             if (provider.equals(LocationManager.GPS_PROVIDER)) {
-                sendBroadcast(BROADCAST_LOCATION_GPS_DISABLED);
+                BroadcastHelper.sendBroadcast(LoggerService.this, BROADCAST_LOCATION_GPS_DISABLED);
             } else if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
-                sendBroadcast(BROADCAST_LOCATION_NETWORK_DISABLED);
+                BroadcastHelper.sendBroadcast(LoggerService.this, BROADCAST_LOCATION_NETWORK_DISABLED);
             }
             if (!locationHelper.hasEnabledProviders()) {
-                sendBroadcast(BROADCAST_LOCATION_DISABLED);
+                BroadcastHelper.sendBroadcast(LoggerService.this, BROADCAST_LOCATION_DISABLED);
             }
         }
 
@@ -326,9 +317,9 @@ public class LoggerService extends Service {
         public void onProviderEnabled(@NonNull String provider) {
             if (Logger.DEBUG) { Log.d(TAG, "[location provider " + provider + " enabled]"); }
             if (provider.equals(LocationManager.GPS_PROVIDER)) {
-                sendBroadcast(BROADCAST_LOCATION_GPS_ENABLED);
+                BroadcastHelper.sendBroadcast(LoggerService.this, BROADCAST_LOCATION_GPS_ENABLED);
             } else if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
-                sendBroadcast(BROADCAST_LOCATION_NETWORK_ENABLED);
+                BroadcastHelper.sendBroadcast(LoggerService.this, BROADCAST_LOCATION_NETWORK_ENABLED);
             }
         }
 
--- app/src/main/java/net/fabiszewski/ulogger/WebSyncService.java
+++ app/src/main/java/net/fabiszewski/ulogger/WebSyncService.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
@@ -197,8 +198,7 @@ public class WebSyncService extends Service {
                 params.put(WebHelper.PARAM_TRACKID, String.valueOf(trackId));
                 web.postPosition(params);
                 db.setSynced(getApplicationContext(), rowId);
-                Intent intent = new Intent(BROADCAST_SYNC_DONE);
-                sendBroadcast(intent);
+                BroadcastHelper.sendBroadcast(this, BROADCAST_SYNC_DONE);
             }
         } catch (IOException e) {
             // handle web errors
@@ -246,9 +246,11 @@ public class WebSyncService extends Service {
         if (Logger.DEBUG) { Log.d(TAG, "[handleError: retry: " + message + "]"); }
 
         db.setError(message);
-        Intent intent = new Intent(BROADCAST_SYNC_FAILED);
-        intent.putExtra("message", message);
-        sendBroadcast(intent);
+
+        Bundle extras = new Bundle();
+        extras.putString("message", message);
+        BroadcastHelper.sendBroadcast(this, BROADCAST_SYNC_FAILED, extras);
+
         // retry only if tracking is on
         if (LoggerService.isRunning()) {
             setPending();
